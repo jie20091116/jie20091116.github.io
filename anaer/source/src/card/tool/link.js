695b7c4c1afbce09ab90d1ded48e7398608fc01d
jie20091116 <106426804+jie20091116@users.noreply.github.com>
2023-07-08 13:11:16 +0800

    Add files via upload

---
+++ anaer/source/src/card/tool/link.js
@@ -0,0 +1,158 @@
+﻿ //地址
+var m_link = require('model/link');
+
+BCD.addEvent('fast_input', function(ele) {
+  let viewInput = ele.find('#content');
+  let viewDrop = ele.find('[data-selector="dropdown"]');
+  let btnNotify = ele.find('#notify');
+  viewDrop.setView({
+    name: 'link/drop',
+    template: '<ul><%(obj ||[]).forEach(function(o, i){%>' +
+      '<li data-on="?m=open" data-url="<%=o.href%>">'+
+      ' <div class="left"><span class="am-badge am-round"><%=i%></span></div>'+
+      ' <div class="right"><a><%=o.name%></a><div class="addr"><%=o.searchHref%></div></div>'+
+      '</li>' +
+      '<%})%></ul>'+
+      '<div class="tips">提示：Tab键打开第一个链接、数字键打开对应链接、上下键选择、回车键打开、Esc键还原</div>',
+    end: function(data) {
+      if (!data.length) {
+        return 'hide';
+      }
+    }
+  });
+  let selectLi = null;
+  let selectList = null;
+  let index = -1;
+  let oldWord = '';
+  const reset = () => {
+    viewDrop.hide();
+    viewInput.val('');
+    selectLi = null;
+    selectList = null;
+    index = -1;
+    oldWord = '';
+    setTimeout(()=>viewInput[0].focus(), 300);
+  };
+  const doSearch = () => {
+    let href = 'https://www.baidu.com/s?wd='+encodeURIComponent(getWord());
+    // let href = 'https://www.ecosia.org/search?q='+encodeURIComponent(getWord());
+    // let href = 'https://duckduckgo.com/?q='+encodeURIComponent(getWord());
+    reset();
+    window.open(href);
+  };
+  const getWord = () => viewInput.val().trim();
+  viewInput.on('blur', function(){
+    setTimeout(function(){
+      viewDrop.hide();
+    }, 300);
+  });
+
+  btnNotify.on('click', function(){
+    setTimeout(function(){
+      let word = getWord();
+      if (word) {
+        $.ajax({
+            url: "https://api2.pushdeer.com/message/push?pushkey=PDU3423TORUEz2NZHKZVIpSlb2ErGJcERTunXneI&text="+encodeURIComponent(word),
+            type: 'get',
+            dataType: 'jsonp',  // 请求方式为jsonp
+            crossDomain: true,
+            data: {},
+            success: function(data) {},
+        });
+        // 放success里 好像没提示, 不搞前端, 不确定原因, 先放外面提示已通知
+        // alert('已通知');
+        $(btnNotify).tip('已通知');
+      }
+    }, 300);
+  })
+  ele.on('keydown', function(e) { //上下选择
+    //console.log(e.keyCode);
+    if(e.keyCode==27){
+      return reset();
+    }
+    if (e.keyCode == 13) {
+      if (selectLi) {
+        selectLi.trigger('click');
+        reset();
+      } else {
+        doSearch();
+      }
+      return;
+    }
+    if(!(selectList && selectList.length)){
+      return;
+    }
+    let word = getWord();
+    if(e.keyCode>95 && e.keyCode < 106 &&(!/^\d+$/.test(word))){ //0-9 是96-105 小键盘的数字键
+      if(selectList[e.keyCode-96]){
+        $(selectList[e.keyCode-96]).trigger('click');
+        setTimeout(reset, 100);
+      }
+      return;
+    }
+    if(e.keyCode>47 && e.keyCode < 58 &&(!/^\d+$/.test(word))){ //0-9 是48-57 大键盘的数字键
+      if(selectList[e.keyCode-48]){
+        $(selectList[e.keyCode-48]).trigger('click');
+        return setTimeout(reset, 100);
+      }
+      return;
+    }
+    if(e.keyCode==9){
+      $(selectList[0]).trigger('click');
+      return setTimeout(reset, 100);
+    }
+
+    if (e.keyCode == 40 || e.keyCode == 38) {
+      if (e.keyCode == 40) {
+        index++;
+        if (index >= selectList.length) {
+          index = 0;
+        }
+      }
+      if (e.keyCode == 38) {
+        index--;
+        if (index <= -selectList.length) {
+          index = 0;
+        }
+      }
+      selectList.css('background-color', '');
+      selectLi = selectList.eq(index);
+      selectLi.css('background-color', '#b2d8fa');
+    }
+  });
+
+  ele.on('input', function(e) { //keypress要慢一拍 keypress input keyup
+    let word = getWord();
+    if (word) {
+
+      if (word == oldWord) {
+        return viewDrop.show();
+      }
+      oldWord = word;
+      let list = m_link.searchDirect(word);
+      if (list.length) {
+        index = -1;
+        selectLi = null;
+        viewDrop.reset(list);
+        selectList = viewDrop.find('li');
+      }else{
+        viewDrop.hide();
+      }
+    } else {
+      viewDrop.hide();
+    }
+  });
+
+});
+var view = $(
+  '<div class="am-container">' +
+  '  <div class="am-margin-lg am-u-sm-11" style="padding:0" data-on="?m=fast_input">' +
+  '   <input id="content" type="text" class="am-form-field" style="ime-mode:active" autofocus="autofocus" placeholder="地址直达（支持拼音首字母）">' +
+  '   <div class="autocomplete-dropdown" data-selector="dropdown" style="display:none"></div>' +
+  '   <input id="notify" type="button" class="" style="" value="通知一下", title="在文本框中输入内容, 然后点击按钮可以通知作者." />' +
+  '  </div>'+
+  '</div>').setView({
+  name: 'link/input'
+});
+
+module.exports = view;
---
+++ anaer/source/src/model/link.js
@@ -0,0 +1,149 @@
+const m_util = require('common/util');
+const m_search = require('helper/search');
+
+let links = [];
+let linkTab = {};
+let count = [];
+
+let getLinks = new Promise((resolve) => {
+  BCD.ajaxCache('./json/link.json', function (data) {
+    if (data.length) {
+      m_util.load(['source/lib/pinyinjs/dict/pinyin_dict_firstletter.js',
+        'source/lib/pinyinjs/pinyinUtil.js'
+      ], () => {
+        links = [];
+        linkTab = [];
+        count = [];
+        data.forEach(o => {
+          let start = links.length;
+          // 如果count[o.groupid]不存在，则初始化为0
+          if(!count[o.groupid]){
+            count[o.groupid] = 0;
+          }
+          (o.rows || []).forEach(str => {
+            // 对同一groupid进行计数
+            count[o.groupid] = count[o.groupid] + 1;
+            // console.info("count[o.groupid]=", count[o.groupid]);
+            let arr = str.split(' | ');
+            let firstLetter = pinyinUtil.getFirstLetter(arr[0], true).join(' ');
+            if (arr.length) {
+              var favicon = '';
+              var url = arr[1];
+              if (url) {
+                var domain = url.split('/');
+                if(domain[2]){
+                  // 方式一: 有些网站没有favicon
+                  //if(url.startsWith('https')){
+                  //  favicon = "https://"+domain[2]+"/favicon.ico";
+                  //}else{
+                  //  favicon = "http://"+domain[2]+"/favicon.ico";
+                  //}
+
+                  // 调整使用获取favicon的公共API
+                  // 方式二: dnspod domain不带http前缀
+                  // favicon = 'https://statics.dnspod.cn/proxy_favicon/_/favicon?domain='+domain[2];
+
+                  // 方式三: google domain最好带http/https前缀, 有些不带取不到favicon
+                  //var newDomain = '';
+                  //if(url.startsWith('https')){ // 暂时先这么处理, 有空再调整
+                  //  newDomain = "https://"+domain[2];
+                  //}else{
+                  //  newDomain = "http://"+domain[2];
+                  //}
+                  //favicon = 'https://www.google.com/s2/favicons?domain='+newDomain;
+
+                  // 方式四： 使用本地的favicon
+                  favicon = 'https://fastly.jsdelivr.net/gh/anaer/anaer.github.io@master/favicon/' + domain[2] + '.ico';
+                }
+              }
+              let item = {
+                // 缩写
+                name: truncate(arr[0], 20),
+                title: arr[0],
+                href: arr[1],
+                favicon: favicon,
+                firstLetter: firstLetter,
+                searchKey: arr.concat(o.title, firstLetter).join(' ')
+              };
+              links.push(item);
+            }
+          });
+          if (o.groupid) {
+            let tab = {
+              title: o.title,
+              rows: links.slice(start),
+              // 如果未设置limit, 则默认为6, 同一组最多显示6条. 防止太长
+              limit: o.limit || 6
+            }
+            if (linkTab[o.groupid]) {
+              linkTab[o.groupid].push(tab);
+            } else {
+              linkTab[o.groupid] = [tab];
+            }
+          }
+        });
+        resolve(data);
+      });
+    }else{
+      resolve(data);
+    }
+    return data.length ? 1 : 0;
+  }, 0, 3E3, 1);
+}).catch((e) => console.info(e));
+
+/**
+ * 截取字符串.
+ *
+ * @param {*} str 字符串
+ * @param {*} limit 限制长度, 如果是中文, 再减4, 主要是中文宽度较宽
+ * @returns
+ */
+function truncate(str, limit) {
+  var chinese = RegExp('[\u4e00-\u9fa5]{0,}');
+  if(str.length > limit){
+    if(chinese.test(str) && (limit-4>0)){
+        limit = limit -4;
+    }
+    return str.slice(0, limit) + '...';
+  }else{
+    return str;
+  }
+}
+
+//搜索直达
+const searchDirect = (word) => {
+  let reg = m_search.getGlobalRegex(word, 'i');
+  let regGlobal = m_search.getGlobalRegex(word, 'ig');
+  //console.log(reg);
+  return links.filter(o => reg.test(o.searchKey)).map(o => {
+    let weight = 0;
+    let item = {
+      href: o.href,
+      name: o.name.replace(regGlobal, function ($0) {
+        weight += 3 * $0.length;
+        return '<span class="am-text-danger">' + $0 + '</span>';
+      })
+    };
+    if (word.length > 1) {
+      item.searchHref = o.href.replace(regGlobal, function ($0) {
+        weight += $0.length;
+        return '<span class="am-text-danger">' + $0 + '</span>';
+      });
+    } else {
+      item.searchHref = o.href;
+    }
+
+    o.firstLetter.replace(regGlobal, function ($0) {
+      weight += 2 * $0.length;
+      return $0;
+    });
+    item.weight = weight
+    return item;
+  }).sort((a, b) => b.weight - a.weight).slice(0, 10);
+};
+module.exports = {
+  getLinks,
+  searchDirect,
+  getLinksSync: () => links,
+  getLinkTabSync: () => linkTab
+};
